b7fdb9595f08db4f61d23737ac8cb1d7ef90a987
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-06 21:58:49 -0500

    completed GuestCard

--- client/src/components/Guests/GuestCard.tsx
+++ client/src/components/Guests/GuestCard.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+// TODO pass guest information
 
 const GuestCard = () => {
   return (
-    <div>
+    <div className="cardContainer">
       <div className="cardName">Name</div>
-      <div className="cardPhoneNumber">Phone Number</div>
-      <div className="cardEmail">Email</div>
+      <div className="cardPhoneNumber">
+        <LocalPhoneIcon fontSize="small" />
+        Phone Number
+      </div>
+      <div className="cardEmail">
+        <EmailIcon fontSize="small" />
+        Email
+      </div>
     </div>
   );
 };
