4620d7e8a28e2e09fcc23244f70b39d3696c68c0
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-08 19:28:35 -0500

    added appointments date selector

--- client/src/components/Appointments/Appointments.tsx
+++ client/src/components/Appointments/Appointments.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import retrieveTodayDate from '../../util/retrieveTodayDate';
 
 const Appointments = () => {
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    const today = retrieveTodayDate();
+    setDate(today);
+  }, []);
+
+  const handleDateChange = (e: any) => {
+    setDate(e.target.value);
+  };
+
   return (
     <div className="appointmentsContainer">
       <div className="cardHeader">Appointments </div>
       <div className="cardSeparator"></div>
+      <input
+        type="date"
+        value={date}
+        name="dateSelector"
+        onChange={(e) => {
+          handleDateChange(e);
+        }}
+      />
       date Selectors
     </div>
   );
---
+++ client/src/util/retrieveTodayDate.tsx
@@ -0,0 +1,11 @@
+const retrieveTodayDate = () => {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  const formattedDate = yyyy + '-' + mm + '-' + dd;
+
+  return formattedDate;
+};
+
+export default retrieveTodayDate;
