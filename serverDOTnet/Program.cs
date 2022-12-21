ccdb83a202c60e3439ab2fe4835e70fce335e005
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-21 18:45:18 -0500

    updated comment

--- serverDOTnet/Program.cs
+++ serverDOTnet/Program.cs
@@ -10,7 +10,7 @@ builder.Services.Configure<MongoDBSettings>(
 builder.Services.AddSingleton<MongoDBService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
