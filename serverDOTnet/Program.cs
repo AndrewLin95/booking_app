5570c69690f50938553994dcc2e6f672a007927a
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-21 18:57:55 -0500

    added services to the program

--- serverDOTnet/Program.cs
+++ serverDOTnet/Program.cs
@@ -1,6 +1,11 @@
+using Mongo.Models;
+using Mongo.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,8 +17,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
