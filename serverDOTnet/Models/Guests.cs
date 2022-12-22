f053c546e3364994ecd0b9e46916f271bac9039a
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-21 19:02:35 -0500

    created the guest model

---
+++ serverDOTnet/Models/Guests.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Mongo.Models;
+
+public class Guests
+{
+  [BsonId]
+  [BsonRepresentation(BsonType.ObjectId)]
+  public string? Id { get; set; }
+
+  public string firstName { get; set; } = null!;
+  public string lastName { get; set; } = null!;
+  public string phoneNumber { get; set; } = null!;
+  public string email { get; set; } = null!;
+}
