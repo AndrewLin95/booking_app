5335f5a31849ff0a0bfd967d43a3e0e1a48734a0
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-25 15:42:48 -0500

    updated guest schema

--- serverDOTnet/Models/Guests.cs
+++ serverDOTnet/Models/Guests.cs
@@ -8,10 +8,10 @@ public class Guests
 {
   [BsonId]
   [BsonRepresentation(BsonType.ObjectId)]
-  public string? Id { get; set; }
+  public string Id { get; set; } = null!;
 
   public string firstName { get; set; } = null!;
   public string lastName { get; set; } = null!;
-  public int phoneNumber { get; set; }
+  public string phoneNumber { get; set; } = null!;
   public string email { get; set; } = null!;
 }
