9d95c98849a66b6acdb46220fa942ed6c478c908
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-26 13:17:46 -0500

    typo

--- serverDOTnet/Program.cs
+++ serverDOTnet/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<GuestsService>();
+builder.Services.AddSingleton<StaffsService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
