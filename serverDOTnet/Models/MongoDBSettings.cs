2edc477e860d821360a61855118f89be87657af1
AndrewLin95 <andrew.lin88@hotmail.com>
2022-12-26 12:51:39 -0500

    updated model

--- serverDOTnet/Models/MongoDBSettings.cs
+++ serverDOTnet/Models/MongoDBSettings.cs
@@ -4,6 +4,6 @@ public class MongoDBSettings
 {
   public string ConnectionURI { get; set; } = null!;
   public string DatabaseName { get; set; } = null!;
-  public string CollectionName { get; set; } = null!;
-
+  public string GuestsCollectionName { get; set; } = null!;
+  public string StaffsCollectionName { get; set; } = null!;
 }
